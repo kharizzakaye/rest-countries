e0a172a2829e06ef5a9e4d477e68300d9ba16a02
Khariza <kharizzakaye@gmail.com>
2024-09-28 20:43:14 +0800

    Card component

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import * as apiService from "../services/apiService";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, CardGroup, Container } from 'react-bootstrap';
 
 const CardComponent = () => {
 
@@ -26,23 +26,31 @@ const CardComponent = () => {
 
 
   return (
-    <>
-       { countriesList && 
-         <>
-           
-          { countriesList.map((country: any, index) => (
-            <div key={index}>
-              <h1>{country.name?.common}</h1>
-              <p>Population: {country.population}</p>
-              <p>Region: {country.region}</p>
-              <p>Capital: {country.capital}</p>
-            </div>
-          ))}
-          
-         </>
+    <Container>
+      { countriesList && 
+        <Row xl={4} lg={3} md={2} sm={2} xs={1} className="g-4">
+     
+            { countriesList.map((country: any, index) => (
+            
+                <Col key={index} >
+                  <Card className="p-0 h-100 border-0">
+                    <Card.Img variant="top" src={country.flags?.png} style={{ height: "200px" }} />
+
+                    <Card.Body>
+                      <Card.Title>{country.name?.common}</Card.Title>
+                      <Card.Text>Population: { Number(country.population).toLocaleString() }</Card.Text>
+                      <Card.Text>Region: {country.region}</Card.Text>
+                      <Card.Text>Capital: {country.capital}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+        
+            ))}
+       
+        </Row>
       
        }
-    </>
+    </Container>
   )
 }
 
