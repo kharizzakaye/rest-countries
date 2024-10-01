d9686b6ea8e4ca1a7209c331dbd5a30a78e02a86
Khariza <kharizzakaye@gmail.com>
2024-10-01 13:59:06 +0800

    Fix filters

--- src/components/SearchBarComponent.tsx
+++ src/components/SearchBarComponent.tsx
@@ -22,7 +22,10 @@ const regionsList = [
 const SearchBarComponent = (props: Props) => { 
     const [countriesList, setCountriesList] = useState<any[]>([]);
     const [unfilteredCountriesList, setUnfilteredCountriesList] = useState<any[]>([]);
+    const [filterRegionValue, setFilterRegionValue] = useState<string>("");
+    const [filterCountryNameValue, setFilterCountryNameValue] = useState<string>("");
 
+    // ALL Filters EMPTY
     async function getAllCountries()
     {
         const getAllCount: any = await apiService.getAllCountriesList();
@@ -30,17 +33,50 @@ const SearchBarComponent = (props: Props) => {
         setUnfilteredCountriesList(getAllCount);
     }
 
+    // Region NOT EMPTY - country EMPTY
+    async function getAllCountriesByRegion(region: string)
+    {
+        const getCountriesByRegion: any = await apiService.getAllCountriesListByRegion(region);
+        setCountriesList(getCountriesByRegion);
+    }
 
     async function onSelectRegionChangeHandler (event: any)
     {
-        if ( event.target.value == "Filter by Region")
+        let filteredData = [];
+        
+        // Region EMPTY
+        if ( event.target.value == "Filter by Region" || event.target.value == "")
         {
-            getAllCountries();
+            setFilterRegionValue("");
+
+            if ( filterCountryNameValue == "") // ALL Filters EMPTY
+            {
+                getAllCountries();
+            }
+            else // Country NOT EMPTY - Region EMPTY
+            {
+                filteredData = unfilteredCountriesList.filter ( (country: any) => country.name?.common.toLowerCase().includes(filterCountryNameValue.toLowerCase()) );              
+                setCountriesList(filteredData);
+            }
         }
-        else
+        else // Region NOT EMPTY
         {
-            const getCountriesByRegion: any = await apiService.getAllCountriesListByRegion(event.target.value);
-            setCountriesList(getCountriesByRegion);
+            setFilterRegionValue(event.target.value);
+            
+            if ( filterCountryNameValue == "") // Region NOT EMPTY - country EMPTY
+            {
+                getAllCountriesByRegion(event.target.value);
+            }
+            else // ALL Filters NOT EMPTY
+            {
+                // filter by region
+                const filteredByRegion = unfilteredCountriesList.filter( (data: any) => data.region.toLowerCase().includes(event.target.value.toLowerCase()) );
+
+                // then filter by keyword
+                filteredData = filteredByRegion.filter ( (country: any) => country.name?.common.toLowerCase().includes(filterCountryNameValue.toLowerCase()) );
+
+                setCountriesList(filteredData);
+            }
         }
     }
 
@@ -48,15 +84,40 @@ const SearchBarComponent = (props: Props) => {
     {
         const keywordSearch = event.target.value;
 
+        // Country EMPTY
         if ( keywordSearch == "" || keywordSearch == null)
         {
-            getAllCountries();
+            setFilterCountryNameValue("");
+
+            if (filterRegionValue == "") // ALL Filters EMPTY
+            {                                                          
+                getAllCountries();
+            }
+            else // Region NOT EMPTY - country EMPTY
+            {
+                getAllCountriesByRegion(filterRegionValue);
+            }
         }
-        else
+        else // Country NOT EMPTY
         {
-            const filteredData = unfilteredCountriesList.filter ( (country: any) => country.name?.common.toLowerCase().includes(keywordSearch.toLowerCase()) )
+            let filteredData = [];
+            setFilterCountryNameValue(keywordSearch);
+
+            if (filterRegionValue == "") // Country NOT EMPTY - Region EMPTY
+            {
+                filteredData = unfilteredCountriesList.filter ( (country: any) => country.name?.common.toLowerCase().includes(keywordSearch.toLowerCase()) );              
+            }
+            else // ALL Filters NOT EMPTY
+            {
+                // filter by region
+                const filteredByRegion = unfilteredCountriesList.filter( (data: any) => data.region.toLowerCase().includes(filterRegionValue.toLowerCase()) );
+
+                // then filter by keyword
+                filteredData = filteredByRegion.filter ( (country: any) => country.name?.common.toLowerCase().includes(keywordSearch.toLowerCase()) );
+            }
+
             setCountriesList(filteredData);
-        } 
+        }
     }
 
     
@@ -91,7 +152,7 @@ const SearchBarComponent = (props: Props) => {
                                     aria-label="Filter by Region dropdown"
                                     onChange={(event: any) => onSelectRegionChangeHandler(event)}
                                 >
-                                    <option>Filter by Region</option>
+                                    <option value="">Filter by Region</option>
                                     {
                                         regionsList.map((region: any, index) => (
                                             <option key={index} value={region.text}>{region.text}</option>
